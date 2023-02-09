2d0d07e86cd8bf087bb27c56cb1ec5911ec19a8d
Ayse <35005099+yukselas@users.noreply.github.com>
2023-02-09 23:44:59 +0300

    Update multiprocess-curl.sh

--- multiprocess-curl.sh
+++ multiprocess-curl.sh
@@ -14,8 +14,8 @@ fi
 
 
 multicurlprocess () {
-	 cmd="curl -s --location --request POST 'https://devk8s.isimplatform.io/isimcore/api/User' \
---header 'Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJMb2dpblRpbWUiOiIyMDIyLTEyLTEzVDE0OjMwOjMwLjcyNDI4NDErMDM6MDAiLCJFeHBpcmVUaW1lIjoiMjAyMy0wMy0xM1QxNDozMDozMC43MjQyODU4KzAzOjAwIiwiTGFzdFJlZnJlc2hlZFRpbWUiOiIyMDIyLTEyLTEzVDE0OjMwOjMwLjcyNDI4NzMrMDM6MDAiLCJQcmlvcml0eSI6MCwiUm9sZXMiOlsxLDIsMyw0LDUsNiw4LDksMTAsMTAwMSwxMDAyLDExMDEsMTEwMiwxMTAzLDExMDQsMTEwNV0sIlJvbGVzVXVpZCI6W10sIlVzZXJHcm91cHNJZHMiOltdLCJVc2VyR3JvdXBzTmFtZXMiOltdLCJSb2xHcm91cHNJZHMiOlsxXSwiRmlyc3RuYW1lIjoiaXNpbSIsIkxhc3RuYW1lIjoiaXNpbSIsIkxvZ2luVHlwZSI6MSwiU2Vzc2lvbklkIjoiNTViZjIzYzItMWI5ZS00MTY4LWI1ODgtZDgyMzE1NjZjMDk2IiwiVG9rZW4iOm51bGwsIlVzZXJJZCI6MSwiVXNlcm5hbWUiOiJpc2ltIiwiUGFzc3dvcmQiOm51bGwsIkNsaWVudCI6eyJJZCI6MSwiVXVpZCI6ImlzaW1fd2ViX2NsaWVudCIsIlBjTmFtZSI6IldlYkNsaWVudCJ9LCJGb3JjZUxvZ2luIjpmYWxzZX0.sILWpRCxu8OdPWvBH-A8aTb8GF1_c5XH-k2QMplc2sE' \
+	 cmd="curl -s --location --request POST 'abc/api/User' \
+--header 'Authorization: .--' \
 --header 'Content-Type: application/json' \
 --data-raw '{"'
     "priority": 0,
@@ -26,7 +26,7 @@ multicurlprocess () {
     "lastname": "ghgh",
     "emailAddress": null,
     "phoneNumber": null,
-    "password": "!2345qawsedrfQ"'"
+    "password": "!234"'"
 }' -o $logdir/$1.output"
     echo $cmd > $logdir/$1.cmd 
     eval $cmd  # eval komutu kendinden sonraki string komutu execute eder
